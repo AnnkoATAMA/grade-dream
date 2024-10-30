a2776513235e0da0d1e8a6b7c1f004fa2066d9f7
AnnkoATAMA <annkopan0903@gmail.com>
2024-10-31 01:10:10 +0900

    lineが動くとwebが止まる問題の解決

--- back/src/routers/line.py
+++ back/src/routers/line.py
@@ -8,6 +8,12 @@ from dotenv import load_dotenv
 from .keiba import get_race_results
 line_router = APIRouter()
 
+racecourse_codes = {
+    "札幌": "01", "函館": "02", "福島": "03", "新潟": "04",
+    "東京": "05", "中山": "06", "中京": "07", "京都": "08",
+    "阪神": "09", "小倉": "10"
+}
+
 # LINE APIの設定
 load_dotenv()
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
@@ -37,6 +43,7 @@ def handle_message(event):
     print(user_message)
     try:
         racecourse, count, race_date, race_num = user_message.replace(" ", "").split(",")
+        racecourse_code = racecourse_codes.get(racecourse)
         print(racecourse, count, race_date, race_num)
     except ValueError:
         line_bot_api.reply_message(
@@ -45,14 +52,14 @@ def handle_message(event):
         )
         return
 
-    send_race_result(event.reply_token, racecourse, count, race_date, race_num)
+    send_race_result(event.reply_token, racecourse_code, count, race_date, race_num)
 
-def send_race_result(reply_token, racecourse, count, race_date, race_num):
-    result = get_race_results(racecourse, count, race_date, race_num)
+def send_race_result(reply_token, racecourse_code, count, race_date, race_num):
+    result = get_race_results(racecourse_code, count, race_date, race_num)
     print(result)
 
     if result:
-        result_text = "\n".join([f"{r['rank']}: {r['name']} (人気: {r['ninki']}, オッズ: {r['odds']})" for r in result])
+        result_text = "\n".join([f"{r['rank']}着 {r['name']} ({r['ninki']}番人気, オッズ: {r['odds']})" for r in result])
         line_bot_api.reply_message(reply_token, TextSendMessage(text=result_text))
     else:
         line_bot_api.reply_message(reply_token, TextSendMessage(text="結果が取得できませんでした。"))
